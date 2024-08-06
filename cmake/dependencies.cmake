a48b14c5eaa71f4a604598b61acd3ad4b4c395d1
Jian Sun <sunjian@ucar.edu>
2024-08-05 21:13:14 -0600

    Add unit and benchmark tests for naive tridiagonal matrix solver (#2)
    * add naive implementation of tridiagonal matrix solver
    
    * add the cmake setups
    
    * add the first unit test
    
    * fix compilation bugs and the test runs
    
    * update the unit tests and add verification
    
    * bug fix for benchmark test and it works now
    
    * reorder the class data and function based on Matt's and Michael's
    comments
    
    * update unit test with new data and function structure
    
    * Add .gitignore file
    
    * update the benchmark tests with new class and function
    
    * add random number generator for initialization
    
    * bug fix for benchmark test setup
    
    * update the setup of random number generator
    
    * remove unused headers
    
    * update the readme file
    
    * update the constructor and function based on Matt's comments
    
    * move verify function to a separate header
    
    * fix compilation bugs and some minor changes
    
    * change location of library definition
    
    * update cmake file with library definition

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,92 @@
+include(FetchContent)
+
+################################
+# Memory check
+
+if(ENABLE_MEMCHECK)
+  if (${GPU_TYPE} STREQUAL "None")
+    # We need to suppress some errors/warnings from valgrind to make it work properly 
+    find_file(MEMCHECK_SUPPRESS_FILE
+        DOC "Suppression file for memory checking"
+        NAMES openmpi-valgrind.supp
+        PATHS
+        /usr/share/openmpi
+        /usr/lib64/openmpi/share
+        /usr/lib64/openmpi/share/openmpi
+        /usr/share)
+    if(MEMCHECK_SUPPRESS_FILE)
+        set(MEMCHECK_SUPPRESS "--suppressions=${PROJECT_SOURCE_DIR}/test/valgrind.supp --suppressions=${MEMCHECK_SUPPRESS_FILE}")
+    else()
+        set(MEMCHECK_SUPPRESS "--suppressions=${PROJECT_SOURCE_DIR}/test/valgrind.supp")
+    endif()
+  endif()
+endif()
+
+################################
+# Google test
+
+if(ENABLE_UNIT_TESTS)
+  FetchContent_Declare(googletest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG be03d00f5f0cc3a997d1a368bee8a1fe93651f48
+  )
+  set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
+  set(BUILD_GMOCK OFF CACHE BOOL "" FORCE)
+  FetchContent_MakeAvailable(googletest)
+endif()
+
+################################
+# Google benchmark
+
+if(ENABLE_BENCHMARK_TESTS)
+  set(BENCHMARK_DOWNLOAD_DEPENDENCIES ON)
+  set(BENCHMARK_ENABLE_TESTING OFF)
+  FetchContent_Declare(
+      benchmark
+      GIT_REPOSITORY https://github.com/google/benchmark.git
+      GIT_TAG v1.8.3
+  )
+  FetchContent_MakeAvailable(benchmark)
+endif()
+
+################################
+# GPU Support
+
+if(NOT ${GPU_TYPE} STREQUAL "None")
+  string(TOLOWER ${GPU_TYPE} GPU_TYPE_LOWER)
+  # Data center GPUs
+  set(cuda_arch_map_v100 70)
+  set(cuda_arch_map_a100 80)
+  set(cuda_arch_map_h100 90)
+  set(cuda_arch_map_h200 90)
+  set(cuda_arch_map_b100 100)
+  set(cuda_arch_map_b200 100)
+  # Consumer grade GPUs
+  set(cuda_arch_map_turing 75)
+
+  set(cuda_arch_map_all_major all-major)
+  # Setting CUDAARCHS does not override CMAKE_CUDA_ARCHITECTURES or CUDA_ARCHITECTURES
+  # until the current process has returned to the caller site in CMake.
+  set(ENV{CUDAARCHS} ${cuda_arch_map_${GPU_TYPE_LOWER}})
+
+  if("$ENV{CUDAARCHS}" STREQUAL "")
+    # dynamically create the current options
+    set(arch_options "")
+    foreach(arch IN ITEMS a100 v100 h100 h200 b100 b200 turing)
+      list(APPEND arch_options ${arch})
+    endforeach()
+    message(FATAL_ERROR "${GPU_TYPE_LOWER} unsupported, current options are ${arch_options}.")
+  endif()
+
+  message(STATUS "GPU architecture found: $ENV{CUDAARCHS}")
+
+  include(CheckLanguage)
+  check_language(CUDA)
+  if(NOT CMAKE_CUDA_COMPILER)
+    message(FATAL_ERROR "Unable to find compatiable compiler for CUDA.")
+  endif()
+  enable_language(CUDA)
+  find_package(CUDAToolkit REQUIRED)
+  set(ENABLE_CUDA ON)
+  set(CUDA_STANDARD_REQUIRED ON)
+endif()
---
+++ include/test_utils.hpp
@@ -0,0 +1,18 @@
+// Copyright (C) 2023-2024 National Center for Atmospheric Research
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <gtest/gtest.h>
+
+// verify two vectors with a relative tolerance
+template<typename T>
+void verify(const std::vector<T>& vector_test, const std::vector<T>& vector_base, T rel_tolerance)
+{
+    ASSERT_EQ(vector_test.size(), vector_base.size());
+    std::size_t vector_size = vector_test.size();
+    for (int i = 0; i < vector_size; i++)
+    {
+        EXPECT_LT(std::abs((vector_test[i] - vector_base[i]) / vector_base[i]), rel_tolerance);
+    }
+}
